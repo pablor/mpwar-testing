a4a77b8f518068531aa5fcc183af4457b69563de
Pablo <pablorg@salleurl.edu>
2014-02-12 20:26:23 +0100

    Dependency ejemplo

---
+++ tests/DependencyFailureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class DependencyFailureTest extends PHPUnit_Framework_TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * @depends testOne
+     */
+    public function testTwo()
+    {
+    }
+}
